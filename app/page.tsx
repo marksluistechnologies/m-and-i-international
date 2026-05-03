ed0effa0541c8e1295e2e36ec91687ef9e9024cd
help-haramexports <help.haramexports@gmail.com>
2026-05-03 17:15:35 +0530

    Initial M&I International Premium 3D Build

--- app/page.tsx
+++ app/page.tsx
@@ -354,7 +354,7 @@ export default function Home() {
                
                {/* Social Icons */}
                <div className="flex space-x-6 mb-4 md:mb-0">
-                  <a href="https://www.facebook.com/m.i.international" target="_blank" rel="noopener noreferrer" className="hover:text-[#D4AF37] transition">
+                  <a href="https://www.facebook.com/profile.php?id=61571956348646&mibextid=rS40aB7S9Ucbxw6v" target="_blank" rel="noopener noreferrer" className="hover:text-[#D4AF37] transition">
                     <svg fill="currentColor" viewBox="0 0 24 24" className="w-4 h-4 md:w-5 md:h-5"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" /></svg>
                   </a>
                   <a href="https://www.instagram.com/m.i.international" target="_blank" rel="noopener noreferrer" className="hover:text-[#D4AF37] transition">
